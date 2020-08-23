510f05c3e7d989390b5bed75cdde5625fbdc148c
Pavel Demin <pavel-demin@outlook.com>
2020-08-23 11:55:59 +0200

    replace _data_end with _sram_data_end

--- common/startup.c
+++ common/startup.c
@@ -3,7 +3,7 @@
 extern uint32_t _stack_end;
 extern uint32_t _flash_start;
 extern uint32_t _sram_start;
-extern uint32_t _data_end;
+extern uint32_t _sram_data_end;
 extern uint32_t _bss_start;
 extern uint32_t _bss_end;
 
@@ -16,7 +16,7 @@ __attribute__((section(".reset_handler"))) void Reset_Handler()
   // copy .text section to RAM
   dst = &_sram_start;
   src = &_flash_start;
-  while(dst < &_data_end) *dst++ = *src++;
+  while(dst < &_sram_data_end) *dst++ = *src++;
 
   // fill .bss section with zeros
   dst = &_bss_start;
